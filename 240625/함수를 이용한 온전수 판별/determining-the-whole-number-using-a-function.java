673d70b762146f8ae27d5a044249b8da65771a19
Manseo Han <103206061+oneofive@users.noreply.github.com>
2024-06-25 23:43:52 +0900

    Title: 함수를 이용한 온전수 판별, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240625/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 240625/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -18,39 +18,10 @@ public class Main {
     }
 
     public static boolean isPerfect(int n) {
-        if (n % 2 == 0 || (n % 3 == 0 && n % 9 != 0)) {
+        if (n % 2 == 0 || n % 10 == 5 || (n % 3 == 0 && n % 9 != 0)) {
             return false;
         }
 
-        if (split(n) == 1) {
-            if (n == 5) {
-                return false;
-            }
-        }
-
-        if (split(n) == 2) {
-            if (n % 10 == 5) {
-                return false;
-            }
-        }
-
-        if (split(n) == 3) {
-            if (n % 100 == 5) {
-                return false;
-            }
-        }
-
         return true;
-    }
-
-    public static int split(int n) {
-        int time = 0;
-
-        while (n > 0) {
-            n /= 10;
-            time++;
-        }
-
-        return time;
     }
 }
