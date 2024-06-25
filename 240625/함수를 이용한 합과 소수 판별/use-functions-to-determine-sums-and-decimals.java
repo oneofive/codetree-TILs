a1c27e15e1afc40a12420b56e9e4877c9c1adfb8
Manseo Han <103206061+oneofive@users.noreply.github.com>
2024-06-25 23:54:02 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240625/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (isPrime(i) && isEvenSumOfDigit(i)) {
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        if (n == 2) {
+            return true;
+        }
+
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isEvenSumOfDigit(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if (sum % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
