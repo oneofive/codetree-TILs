d567c36c4041eb9f69efdb2238e8ae84446bf55e
Manseo Han <103206061+oneofive@users.noreply.github.com>
2024-07-03 23:52:57 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240703/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.print(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
